e9665846dea7b0dc4ffd9317bc852e59ea9f4faa
niftg <niftg@users.noreply.github.com>
2020-07-09 20:03:57 +0000

    split

--- preproc/nj_tpdcsvproc.js
+++ preproc/nj_tpdcsvproc.js
@@ -7,19 +7,27 @@ let l = csvParse(f.toString(), {skip_empty_lines:true})
 let otmd = {words: l.map(pokiNimi)}
 
 function pokiNimi(v, index) {
-    let wordForms = v[0].split(/\s+or\s+/)
+    const wordForms = v[0].split(/\s+or\s+/)
+    const transTypes = v[1].split('\n').map(str=>
+        str.match(/(PRE\s?-\s?VERB||[A-Z]+)\s(.+)/).slice(1)
+    ).map(v=>[
+        v[0].replace(/PRE\s?-\s?VERB/,"PRE-VERB"),
+        v[1].split(v[1].match(/^\(.*\)$/)?null:/;\s*/)
+    ])
+
     let word = {
         entry: {
             id: index+1,
             form: wordForms[0]
         },
-        translations: v[1].split('\n').map(str=>{
-            let m = str.match(/(PRE\s?-\s?VERB||[A-Z]+)\s(.+)/)
+        translations: transTypes.map(v=>v[1].map(str=>{
             return {
-                title: m[1].replace(/PRE\s?-\s?VERB/,"PRE-VERB"),
-                forms: [m[2]]
+                title: v[0],
+                forms: str.split(str.match(/\(.*\,.*\)/)?null:/\s*\,\s*/).map(s=>
+                    v[0].match("VERB")?s.replace(/^to\s*/,''):s
+                )
             }
-        })
+        })).reduce((a,c)=>a.concat(c),[])
     }
     if(wordForms[1]){
         word.variations = [{
