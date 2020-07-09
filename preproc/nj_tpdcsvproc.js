99860323474c6096398bba95c2454b04b4594b3d
niftg <niftg@users.noreply.github.com>
2020-07-09 17:36:29 +0000

    rewrite js

--- preproc/nj_tpdcsvproc.js
+++ preproc/nj_tpdcsvproc.js
@@ -4,29 +4,22 @@ const fs = require('fs')
 let f = fs.readFileSync("./dictionary.csv") // pali sin tan lipu "dictionary.ods"
 let l = csvParse(f.toString(), {skip_empty_lines:true})
 
-let otmd = {}
-otmd["words"] = []
+let otmd = {words: l.map(pokiNimi)}
 
-l.forEach(panaTawaPokiOtmd)
-
-function panaTawaPokiOtmd(v, index) {
-
-    let trans = v[1].split('\n').map(str=>{
-        let m = str.match(/(PRE\s?-\s?VERB||[A-Z]+)\s(.+)/)
-        return {
-            title: m[1].replace(/PRE\s?-\s?VERB/,"PRE-VERB"),
-            forms: [m[2]]
-        }
-    })
-
-    let w = {
+function pokiNimi(v, index) {
+    return {
         entry: {
             id: index+1,
             form: v[0].replace(/\s+or\s+/," or ")
         },
-        translations: trans
+        translations: v[1].split('\n').map(str=>{
+            let m = str.match(/(PRE\s?-\s?VERB||[A-Z]+)\s(.+)/)
+            return {
+                title: m[1].replace(/PRE\s?-\s?VERB/,"PRE-VERB"),
+                forms: [m[2]]
+            }
+        })
     }
-    otmd["words"].push(w)
 }
 
 fs.writeFileSync("./dictionary.csv.nj.json",JSON.stringify(otmd))
